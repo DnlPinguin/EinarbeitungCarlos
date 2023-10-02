0cb13d9cd42d4e4cf8374c67b5e5eb230cb678d4
marcocarlos2375 <idrisscarlosid1@gmail.com>
2023-10-02 10:56:20 +0200

    update webpackConfig

--- src/dashboardSimple.js
+++ src/dashboardSimple.js
@@ -15,9 +15,6 @@ function renderWidget(el) {
 	paragraph.textContent = t('doorman', 'You can define the frontend part of a widget with plain Javascript.')
 	el.append(paragraph)
 
-	const paragraph2 = document.createElement('p')
-	paragraph2.textContent = t('doorman', 'Here is the list of files in your gif folder:')
-	el.append(paragraph2)
 
 	const list = document.createElement('ul')
 	list.classList.add('widget-list')
